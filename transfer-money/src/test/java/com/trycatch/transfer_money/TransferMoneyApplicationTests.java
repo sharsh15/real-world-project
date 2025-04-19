d93cf6f4fdaaaecf7b4a875151da28b89e0409af
Harsh <sharsh1510@gmail.com>
2025-04-19 17:44:26 +0530

    transfer money files

---
+++ transfer-money/src/test/java/com/trycatch/transfer_money/TransferMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trycatch.transfer_money;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferMoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
